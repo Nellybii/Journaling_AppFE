c3f31642766af58de3658641fd51e8a9add9eeef
Nellybii <nellychepkem2015@gmail.com>
2025-04-12 15:17:44 +0300

    updated url

--- journaling_app/settings.py
+++ journaling_app/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'journaling_app.urls'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://journals-entry-git-main-nellybiis-projects.vercel.app/',
+    'https://journals-entry.vercel.app/',
 ]
 
 TEMPLATES = [
